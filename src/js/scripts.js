9ae8c84b010ca0bb8f222d9d5f60a608b85bb53d
Vero Leon <ronni.leon@gmail.com>
2015-03-09 13:44:09 -0700

    instead of build folder, build files will be in root. In this commit the map does show up

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,13 @@
-(function () {
-	'use strict';
-	var Map = function() {};
+var Map = function() {};
 
-	var ViewModel = function() {};
-}());
+var ViewModel = function() {};
+
+function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
