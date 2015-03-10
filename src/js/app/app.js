0357fad238b7605b53253a36647601d5466724fd
Vero Leon <ronni.leon@gmail.com>
2015-03-10 16:56:21 -0700

    Added foundation to project and set the search control on top of map

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -5,7 +5,18 @@ var ViewModel = function() {};
 function initialize() {
 	var mapOptions = {
 		center: { lat: 27.4950000, lng: -109.969000},
-		zoom: 15
+		zoom: 15,
+		panControl: false,
+		streetViewControl: false,
+		zoomControl: true,
+		overviewMapControl: false,
+		mapTypeControlOptions: {
+			style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+			mapTypeIds: [
+		        google.maps.MapTypeId.ROADMAP,
+		        google.maps.MapTypeId.TERRAIN
+		      ]
+		    }
 	};
 	var map = new google.maps.Map(document.getElementById('map-canvas'),
 		mapOptions);
