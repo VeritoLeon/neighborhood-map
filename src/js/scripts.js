8c89972ad135dab129d2fa72ab5ade96d78e9b5a
Vero Leon <ronni.leon@gmail.com>
2015-03-10 14:47:48 -0700

    fixed whitespaces and indents inconsistencies

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,11 +3,11 @@ var Map = function() {};
 var ViewModel = function() {};
 
 function initialize() {
-        var mapOptions = {
-          center: { lat: 27.4950000, lng: -109.969000},
-          zoom: 15
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+	var mapOptions = {
+		center: { lat: 27.4950000, lng: -109.969000},
+		zoom: 15
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+		mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
