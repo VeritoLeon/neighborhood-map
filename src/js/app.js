5e1ae6d54807deeca7c818cc3292f5011f99e0ed
Vero Leon <ronni.leon@gmail.com>
2015-05-04 08:01:27 -0700

    added wikipedia article for the stadium

--- src/js/app.js
+++ src/js/app.js
@@ -102,7 +102,7 @@ function initialize() {
 
 	// All the locations that will be put in the map
 	initialLocations  = [
-		new Location('Lockers', 'Sports restaurant and bar', 27.493913, -109.974022, type.food, {})
+		new Location('Lockers', 'Sports restaurant and bar located in Arena Itson', 27.493913, -109.974022, type.food, {})
 		,new Location('Kiawa', 'University\'s restaurant', 27.493560, -109.972613, type.food, {})
 		,new Location('Doña Magui', 'Homemade food', 27.490329, -109.972748, type.food, {})
 		,new Location('Comedor ITSON', 'University\'s restaurant', 27.491831, -109.970547, type.food, {})
@@ -110,7 +110,7 @@ function initialize() {
 		// ,new Location('Gusto Frio Mr. Brown', 'Ice cream shop', 27.492788, -109.961114, type.food, {})
 		,new Location('Laguna del Nainari', 'Lagoon known as Ciudad Obregon\'s bride', 27.497699, -109.969851, type.nature, {'wikipediaId': '2254604', 'foursquareId': '4cf561ec71538cfa6bdcae2e'})
 		,new Location('Parque infantil Ostimuri', 'City\'s largest park', 27.493909, -109.966797, type.recreation, {})
-		,new Location('Tomas Oroz Gaytan Stadium', 'Baseball stadium', 27.492747, -109.954472, type.recreation, {})
+		,new Location('Tomas Oroz Gaytan Stadium', 'Baseball stadium', 27.492747, -109.954472, type.recreation, {'wikipediaId': '4771088'})
 	];
 
 	// And we bind to our view model
@@ -314,6 +314,7 @@ var ViewModel = function() {
 		listSwitcher.checked = false;
 		self.queryResultsShown(false);
 		self.loadDetails(location);
+		self.setActiveInfo();
 		self.showDetails(true);
 	};
 
