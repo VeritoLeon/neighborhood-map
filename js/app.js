41c46b66265d11354c701434b0b2735bf9a748bf
Vero Leon <ronni.leon@gmail.com>
2015-05-08 09:17:10 -0700

    Added more locations

--- js/app.js
+++ js/app.js
@@ -73,12 +73,12 @@ window.onload = loadGoogleMapsScript;
      */
      type = {
         'food': {
-            ,'marker': pin.setColor('#dd4229')
+            'marker': pin.setColor('#dd4229')
         }
         ,'entertainment': {
             'marker': pin.setColor('#3aa0ef')
         }
-        ,'nature': {
+        ,'outdoors': {
             'marker': pin.setColor('#20be8c')
         }
         , 'recreation': {
@@ -97,17 +97,19 @@ window.onload = loadGoogleMapsScript;
     // All the locations that will be put in the map
     initialLocations  = [
     new Location('Lockers', 'Sports restaurant and bar located in Arena Itson', 27.493913, -109.974022, type.food, {'foursquareId': '5254ea73498ebbc7b795c436'})
-    // ,new Location('Parque Casa Blanca', '', 27.493913, -109.974022, type.nature, {'foursquareId': '4fa9d9f9e4b04de174340ffc'})
-    // ,new Location('Mustang Ranch', '', 27.493913, -109.974022, type.recreation, {'foursquareId': ''})
-    // ,new Location('Alberca Olimpica', '', 27.493913, -109.974022, type.recreation, {'foursquareId': ''})
-    // ,new Location('Cancha de Tenis', '', 27.493913, -109.974022, type.recreation, {'foursquareId': ''})
+    ,new Location('Arena ITSON', 'Arena that holds cultural, sports and academyc events', 27.493730, -109.974260, type.recreation, {'foursquareId': '4fa9d9f9e4b04de174340ffc'})
+    ,new Location('Parque Casa Blanca', 'Casa Blanca\'s park', 27.488502, -109.980858, type.outdoors, {'foursquareId': '4fa9d9f9e4b04de174340ffc'})
+    ,new Location('Extensión de la Cultura', 'University\'s culture building', 27.491421, -109.972339, type.recreation, {'foursquareId': '4cc5c26738aaa09367b51a62'})
+    ,new Location('Piscina Olímpica ITSON', 'Olympic pool', 27.493510, -109.973391, type.outdoors, {'foursquareId': '4dd3c51f1f6e5374d6e0ebce'})
+    ,new Location('Cancha de Tenis', 'Tenis court', 27.492445, -109.972635, type.outdoors, {'foursquareId': '4e556fb6aeb75a7448e0cdb6'})
     ,new Location('Kiawa', 'University\'s restaurant', 27.493560, -109.972613, type.food, {'foursquareId': '4c8178dfd4e23704f0485e88'})
     ,new Location('Doña Magui', 'Homemade food', 27.490329, -109.972748, type.food, {'foursquareId': '5064bc59e4b053bfe4b7885f'})
     ,new Location('Comedor ITSON', 'University\'s restaurant', 27.491831, -109.970547, type.food, {'foursquareId': '4cb87c3ef50e224bd00ae7fb'})
     ,new Location('Cafeteria ITSON', 'University\'s restaurant', 27.492045, -109.969547, type.food, {'foursquareId': '4eb340be0aaf1abede5d0706'})
     ,new Location('Gusto Frio Mr. Brown', 'Ice cream shop', 27.492788, -109.961114, type.food, {'foursquareId': '4ce5cad3678aa093ca97d8ea'})
-    ,new Location('Laguna del Nainari', 'Lagoon known as Ciudad Obregon\'s bride', 27.497699, -109.969851, type.nature, {'wikipediaId': '2254604', 'foursquareId': '4cf561ec71538cfa6bdcae2e'})
-    ,new Location('Parque infantil Ostimuri', 'City\'s largest park', 27.493909, -109.966797, type.recreation, {'foursquareId': '4cc46dc701fb236a19d1abba'})
+    ,new Location('Laguna del Nainari', 'Lagoon known as Ciudad Obregon\'s bride', 27.497699, -109.969851, type.outdoors, {'wikipediaId': '2254604', 'foursquareId': '4cf561ec71538cfa6bdcae2e'})
+    ,new Location('Casa de la cultura de Cajeme', 'Municipal culture house', 27.498726, -109.967483, type.recreation, {'foursquareId': '4cd2f8a383e0721e0b1b5a97'})
+    ,new Location('Parque infantil Ostimuri', 'City\'s largest park', 27.493909, -109.966797, type.outdoors, {'foursquareId': '4cc46dc701fb236a19d1abba'})
     ,new Location('Tomas Oroz Gaytan Stadium', 'Baseball stadium', 27.492747, -109.954472, type.recreation, {'wikipediaId': '4771088', 'foursquareId': '4c1485fda9c220a1f3c6579d'})
     ];
 
@@ -223,7 +225,7 @@ window.onload = loadGoogleMapsScript;
     };
 
     self.showOutdoorsLocations = function() {
-        self.showLocationsByKind(type.nature);
+        self.showLocationsByKind(type.outdoors);
     };
 
     self.showLocationsByKind = function(kind) {
@@ -246,7 +248,7 @@ window.onload = loadGoogleMapsScript;
     }, self);
 
     self.isFilterOutdoors = ko.pureComputed(function() {
-        return self.isFilterActive(type.nature);
+        return self.isFilterActive(type.outdoors);
     }, self);
 
     self.isFilterActive = function(expectedFilter) {
